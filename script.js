b7803923f3ac8d93074acef6b390efe37735fda9
SK <stephanie@sfdigitalstudio.com>
2020-01-15 20:09:31 -0800

    added copy function and while loops

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
+// start a function to generate a random password
+
 function generatePassword() {
 
   // 1. prompt user for the length of a password want to generate
+  //  ????? Is it "better" to use checkboxes and ask all at once versus prompts?  Maybe developer preference?
 
   // stores password length entered by user
-  var userPWLength = prompt("What is the length of the password to be generated?");
+  let userPWLength = prompt("What is the length of the password to be generated?");
 
   // 2. the length must be 8-128 characters.  If number entered is less than 8 or more than 128, then alert the valid password lengths
 
   // If the password length is not between 8 & 128, then prompt again.
-  // **** BONUS - continually check until a valid length is entered
-
+  // continually check until a valid length is entered
 
   // ???????? Why if I use let inside if statements the variable is not the same color as the other variables? 
   // If I change to var then the color matches for all variables
 
-  if (userPWLength < 8 || userPWLength > 128) {
-    var userPWLength = prompt("The length has to be between 8 and 128.");
+  // **** Currently stays in while loop even when click cancel at first prompt.
+  while (userPWLength < 8 || userPWLength > 128) {
+
+    // don't need to use let again as the code "errored" if use let again.
+     userPWLength = prompt("The length has to be between 8 and 128.");
   }
 
   // 3. prompt user what character types want in the password.  Optional: special characters
@@ -30,12 +35,14 @@ function generatePassword() {
   var lowerChar = confirm("Do you want lower case characters in your password?");
   // confirm uppercase values
   var upperChar = confirm("Do you want upper case characters in your password?");
+  // confirm special characters
+  var specialChar = confirm("Do you want special characters in your password?");
 
   // ***** Optional: special characters
 
   // 4. user MUST select at least 1 character type.  If all false, then alert and retry
-  // **** BONUS - continually check until at least one character type is true
-  if (numChar === false && lowerChar === false && upperChar === false) {
+  // continually check until at least one character type is true
+  while (numChar === false && lowerChar === false && upperChar === false && specialChar ===false) {
 
     // confirm numeric values
     var numChar = confirm("You must choose at least one character type.  Do you want numbers in your password?");
@@ -43,60 +50,67 @@ function generatePassword() {
     var lowerChar = confirm("Do you want lower case characters in your password?");
     // confirm uppercase values
     var upperChar = confirm("Do you want upper case characters in your password?");
+     // confirm special characters
+    var specialChar = confirm("Do you want special characters in your password?");
   }
 
-
-  // let userPWLength = 6;
-  // let numChar = true;
-  // let lowerChar = false;
-  // let upperChar = true;
-
   // 5. generate password based on length and character type(s) chosen
-  // got general code from 
-  // https://stackoverflow.com/questions/9719570/generate-random-password-string-with-requirements-in-javascript/9719815
 
-  let numberChars = "0123456789";
-  let upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  let lowerChars = "abcdefghijklmnopqrstuvwxyz";
+  // ?????? better to use string or array?
+  // which option is slower/faster (runtime) to process and/or takes up more/less storage (space complexity)
 
-  // var allChars = numberChars + upperChars + lowerChars;
-
-  // minumum  and at least 1 number and 1 low
+  let numberCharsList = "0123456789";
+  let upperCharsList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let lowerCharsList = "abcdefghijklmnopqrstuvwxyz";
+  let specialCharsList = "!#$%&()*+-/:<=>?@[^{|}~"; // didn't use the enter list
 
   // if num // add 1 number to the pass and concat allchar + numbers
   // if upp // add 1 upp to pass and concat allchar + upp
   // if low // add 1 low to pass and concat allchar + low
 
-  // allchar has  with all the posibilities 
+  // set password to an array as can shuffle what get later
+  // set allChar which is an array of possible values
   var password = []
   var allChar = []
+
+  // see if user picked a number value.  If so, then put a number in the password
   if (numChar) {
-    allChar = allChar + numberChars
-    password.push(generateChar(numberChars))
+    allChar = allChar + numberCharsList
+    password.push(generateChar(numberCharsList))
   }
+  // see if user picked a lower char value.  If so, then put a lower char in the password
   if (lowerChar) {
-    allChar = allChar + (lowerChars)
-    password.push(generateChar(lowerChars))
+    allChar = allChar + (lowerCharsList)
+    password.push(generateChar(lowerCharsList))
   }
+    // see if user picked an upper char value.  If so, then put an upper char in the password
   if (upperChar) {
-    allChar = allChar + (upperChars)
-    password.push(generateChar(upperChars))
+    allChar = allChar + (upperCharsList)
+    password.push(generateChar(upperCharsList))
   }
-  console.log("1'", password)
-  console.log("posibiities", allChar)
+  // see if user picked a special char value.  If so, then put a special char in the password
+  if (specialChar) {
+    allChar = allChar + (specialCharsList)
+    password.push(generateChar(specialCharsList))
+  }
+  // console.log("1'", password)
+  // console.log("posibiities", allChar)
 
-  // fill the rest until the pass lenght
-  //loop
-  var start = password.length
+  // set a variable foer the password length
+  let start = password.length
 
-  console.log(userPWLength, start);
+  // fill the rest of the password array until the password length
+  // console.log(userPWLength, start);
   for (var i = start; i < userPWLength; i++) {
     password.push(generateChar(allChar))
   }
-  console.log("PASS", password)    // password is an array, you need a string 
+  // console.log("PASS", password)  
 
   // shuffle before make to a string
 
+
+
+  // .join("") turns an array in to a string
   return password.join("")
  
 
@@ -109,8 +123,7 @@ function generateChar(arr) {
 
 }
 
-
-// function shuffleArray(array) {
+// function shuffleArray(password) {
 //   for (let i = array.length - 1; i > 0; i--) {
 //     let j = Math.floor(Math.random() * (i + 1));
 //     let temp = array[i];
@@ -126,6 +139,8 @@ function generateChar(arr) {
 //   return array;
 // }
 
+
+
 // 6. show generated password on the screen (first attempt can be alert)
 
 
@@ -134,6 +149,8 @@ function generateChar(arr) {
 // the first element in the document with the ID "generate" is returned:
 var generateBtn = document.querySelector("#generate");
 
+// create a variable for the copy button using the ID 
+var copyBtn = document.querySelector("#copy");
 
 // Write password to the #password input
 function writePassword() {
@@ -142,7 +159,7 @@ function writePassword() {
 
   passwordText.value = password;
 
-  // removes the disabled attribute in HTML so now the button is clickable
+  // removes the disabled attribute in HTML so now the button is clickable after a password is generated
   copyBtn.removeAttribute("disabled");
   copyBtn.focus();
 }
@@ -155,9 +172,25 @@ function copyToClipboard() {
   // select ID password and execute commands
   // find i execute the copy command
 
+  //  Get the password field 
+    var copyText = document.getElementById("password");
+  
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+  
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+  
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText.value);
+
 }
 
 // Add event listener to generate button
+// When the generate button is clicked, then run the writePassword function.  Within it is the generatPassword function
 generateBtn.addEventListener("click", writePassword);
 
 // BONUS EVENT LISTENER
+// added on click event to the copy button
+copyBtn.addEventListener("click", copyToClipboard);
